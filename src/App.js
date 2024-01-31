00007850d5c425a28931112502a0deabd0580846
Divashka <dianakuzmina325@gmail.com>
2024-01-31 18:15:11 +0300

    Реализация счетчика

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './index.scss';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+
+ const [count, setCount] = useState(0);
+
+ const onClickPlus = () => {
+  setCount(count + 1)
+ }
+
+ const onClickMinus = () => {
+  setCount(count - 1)
+ }
+
+ return (
+  <div className="App">
+   <div>
+    <h2>Счетчик:</h2>
+    <h1>{count}</h1>
+    <button onClick={onClickMinus} className="minus">- Минус</button>
+    <button onClick={onClickPlus} className="plus">Плюс +</button>
+   </div>
+  </div>
+ );
 }
 
 export default App;
